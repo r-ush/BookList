263428f194780aa24c0e947ca6bff2d04c5bb82f
aarush <aarush.bhatt@gmail.com>
2020-06-28 12:09:17 +0530

    added book context provider

--- src/contexts/BookContext.jsx
+++ src/contexts/BookContext.jsx
@@ -1,9 +1,26 @@
 import React, { createContext, useState } from 'react';
+import { v4 as uuidv4 } from 'uuid';
 
 export const BookContext= createContext()
 
 const BookContextProvider=(props)=>{
-    useState([
-        
+    const [books, setBooks]=useState([
+        {title:'Name of the wind', author:'Patrick Rothfuss', id:1},
+        {title:'The Final Empire', author:'Brandon Sanderson', id:2},
     ])
+
+    const addBook=(title,author)=>{
+        setBooks([...books, {title, author, id:uuidv4()}])
+    }
+
+    const removeBook=(id)=>{
+        setBooks(books.filter(book => book.id !== id))
+    }
+    return(
+        <BookContext.Provider value={{books, addBook, removeBook}}>
+            {props.children}
+        </BookContext.Provider>
+    )
 }
+
+export default BookContextProvider;
