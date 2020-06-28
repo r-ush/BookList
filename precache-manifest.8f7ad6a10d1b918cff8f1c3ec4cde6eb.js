7efb9ec81484f777422bb7d1ba213075a5d75aa0
aarush <aarush.bhatt@gmail.com>
2020-06-28 17:19:27 +0530

    Updates

---
+++ precache-manifest.8f7ad6a10d1b918cff8f1c3ec4cde6eb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85db15c830ca147105d3d080638b5a19",
+    "url": "/BookList/index.html"
+  },
+  {
+    "revision": "5972845f22ee4295ed6d",
+    "url": "/BookList/static/css/main.f7e1efb5.chunk.css"
+  },
+  {
+    "revision": "9ceed3fe243bc46b1e8f",
+    "url": "/BookList/static/js/2.9c2bbd9a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/BookList/static/js/2.9c2bbd9a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5972845f22ee4295ed6d",
+    "url": "/BookList/static/js/main.2d371f87.chunk.js"
+  },
+  {
+    "revision": "6801ac5c186d632b29f5",
+    "url": "/BookList/static/js/runtime-main.d48b8508.js"
+  }
+]);
